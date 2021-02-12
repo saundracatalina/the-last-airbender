02336833a5b94f2a7c7ce0b12dad79b0f10ceb54
Saundra <saundracatalina@gmail.com>
2021-02-12 09:27:11 -0700

    Add initial test for clicking on Search For Members and redirecting

---
+++ spec/features/index_spec.rb
@@ -0,0 +1,12 @@
+require 'rails_helper'
+
+describe 'welcome page' do
+  it 'can select Fire Nation, click search, and be redirected to search path' do
+    visit root_path
+
+    select 'Fire Nation', from: :nation
+    click_on 'Search For Members'
+
+    expect(current_path).to eq(search_path)
+  end
+end
