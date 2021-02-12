035c9d99e342e903c0607a49a96ac1006a89bc4a
Saundra <saundracatalina@gmail.com>
2021-02-12 10:09:43 -0700

    Change uri API call back, but with pagination

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -3,7 +3,7 @@ class SearchController < ApplicationController
     nation = params[:nation]
 
     conn = Faraday.new(url: "https://last-airbender-api.herokuapp.com/api/v1")
-    response = conn.get("characters?perPage=500?page=1")
+    response = conn.get("characters?perPage=100?page=1&affiliation=Fire Nation")
     json = JSON.parse(response.body, symbolize_names: true)
   end
 end
