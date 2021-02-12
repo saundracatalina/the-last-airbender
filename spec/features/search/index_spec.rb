e6004ccb0fbcb02edb495845d4dacfe1b6079aa2
Saundra <saundracatalina@gmail.com>
2021-02-12 10:54:18 -0700

    Add content to view

--- spec/features/search/index_spec.rb
+++ spec/features/search/index_spec.rb
@@ -5,17 +5,18 @@ describe 'search_path for nations' do
     visit root_path
     select 'Fire Nation', from: :nation
     click_button 'Search For Members'
-    expect(current_path).to eq(search_path)
 
+    expect(current_path).to eq(search_path)
     expect(page).to have_content("Number of people in fire_nation: 97")
   end
 
   it 'can see a list with detailed info for the first 25 members of Fire Nation' do
-
+    visit root_path
+    select 'Fire Nation', from: :nation
+    click_button 'Search For Members'
+    # within("#member-#{}") do
+      expect(page).to have_content("Member Name: Afiko")
+    # expect(page).to have_content("Allies: ")
+    # end
   end
-#   And for each of the members I should see:
-# - The name of the member (and their photo, if they have one)
-# - The list of allies or "None"
-# - The list of enemies or "None"
-# - Any affiliations that the member has
 end
